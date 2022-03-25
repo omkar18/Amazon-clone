83f12209af7d987c0c96e56e05104f45a9804157
omkar walujkar <omkarwalujkar18@gmail.com>
2022-03-25 18:54:12 +0530

    [x]- updated gitignore file

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDlQKRixG36AEK0OQwd0Y3hz_iBo5zzz3s",
-  authDomain: "clone-affba.firebaseapp.com",
-  projectId: "clone-affba",
-  storageBucket: "clone-affba.appspot.com",
-  messagingSenderId: "988473910855",
-  appId: "1:988473910855:web:b93ecf1d301c46c50df362",
-  measurementId: "G-KSJ6KVN8WF",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
